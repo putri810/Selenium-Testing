d9e6779021f2eedc2d61976797c1cc7113c53c7f
Zahwa Putri Hamida <putriza810@gmail.com>
2025-06-16 21:56:27 +0700

    selenium task testing

---
+++ test/src/test/java/selenium/locator/SeleniumIntroductionTest.java
@@ -0,0 +1,87 @@
+package selenium.locator;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SeleniumIntroductionTest {
+    private WebDriver driver;
+    
+    @BeforeTest
+    public void setup() throws InterruptedException{
+         // Setup WebDriver
+        System.setProperty("webdriver.chrome.drive", "D:\\Testing-Selenium\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void testloginScenarioTest() throws InterruptedException{
+
+        /*
+         * Steps:
+         * 1. User opens the browser and navigates to the login page
+         * 2. User enters the username and password
+         * 3. User clicks the login button
+         * 4. User is redirected to the home page
+         */
+
+         WebElement userName = driver.findElement(By.id("user-name"));
+         userName.sendKeys("standard_user");
+
+
+         driver.findElement(By.name("password")).sendKeys("secret_sauce");
+
+         WebElement loginButton = driver.findElement(By.className("btn_action"));
+         loginButton.click();
+         Thread.sleep(5000);
+
+         // redirect to home page
+         String name = driver.findElement(By.xpath("//div[@class = 'product_label']")).getText();
+
+         System.out.println("This is main menu: " + name);
+    }
+
+    @Test
+    public void incorrectPasswordTest() throws InterruptedException{
+        /*
+         * Steps:
+         * 1. User opens the browser and navigates to the login page
+         * 2. User enters the username and invalid password
+         * 3. User clicks the login button
+         * 4. User will get error message
+         */
+
+        WebElement userName = driver.findElement(By.id("user-name"));
+         userName.sendKeys("standard_user_failed");
+
+
+         driver.findElement(By.name("password")).sendKeys("secret_sauce");
+
+         WebElement loginButton = driver.findElement(By.className("btn_action"));
+         loginButton.click();
+         Thread.sleep(5000);
+        
+         // verify error message
+         WebElement errorElement = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/h3"));
+         String errorMessage = errorElement.getText();
+
+         System.out.println("This is an error message: " + errorMessage);
+    }
+
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
---
+++ test/src/test/java/selenium/selenium_scenario/Login.java
@@ -0,0 +1,80 @@
+package selenium.selenium_scenario;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class Login {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Testing-Selenium\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void validCredentials() throws InterruptedException {
+        System.out.println("Valid credentials test is running.");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        Thread.sleep(2000);
+
+        // Cek apakah berhasil login ke halaman produk
+        String title = driver.findElement(By.xpath("//span[@class='title']")).getText();
+        Assert.assertEquals(title, "Products", "Title Home text does not match!");
+    }
+
+    @Test(dataProvider = "invalidCredentialsData")
+    public void invalidCredentials(String username, String password, String expectedError) throws InterruptedException {
+        System.out.println("Invalid credentials test is running.");
+
+        driver.findElement(By.id("user-name")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("login-button")).click();
+
+        Thread.sleep(1000);
+
+        if (isElementPresent(By.xpath("//h3[@data-test='error']"))) {
+            String actualError = driver.findElement(By.xpath("//h3[@data-test='error']")).getText();
+            Assert.assertEquals(actualError, expectedError, "Error message does not match!");
+        } else {
+            Assert.fail("Error message not displayed!");
+        }
+    }
+
+    @DataProvider(name = "invalidCredentialsData")
+    public Object[][] invalidCredentialsData() {
+        return new Object[][] {
+            {"standard_user", "wrong_password", "Epic sadface: Username and password do not match any user in this service"},
+            {"wrong_user", "secret_sauce", "Epic sadface: Username and password do not match any user in this service"},
+            {"standard_user", "", "Epic sadface: Password is required"},
+            {"", "secret_sauce", "Epic sadface: Username is required"},
+            {"", "", "Epic sadface: Username is required"}
+        };
+    }
+
+    public Boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
